4e6fae03348656bb91a8f50623d3fd2e662f0d5c
Hadi AL-Hakeem <hadinawar@hotmail.com>
2020-04-16 18:42:49 -0400

    Added new file

--- CreateTable.py
+++ CreateTable.py
@@ -1,6 +1,6 @@
 import boto3
 
-dynamodb = boto3.resource('dynamodb', region_name='us-east-2', endpoint_url="http://localhost:8000")
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
 
 
 table = dynamodb.create_table(
@@ -32,4 +32,4 @@ table = dynamodb.create_table(
     }
 )
 
-print("Table status:", table.table_status)
+print("Table status: ", table.table_status)
